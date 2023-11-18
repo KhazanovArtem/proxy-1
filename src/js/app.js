96369138bdade9818946193f67c8dfe78937d788
Artem Khazanov <tulenka04@gmail.com>
2023-11-18 13:27:42 +0200

    First commit

---
+++ src/js/app.js
@@ -0,0 +1,17 @@
+// TODO: write your code here
+import Bowerman from './Bowerman';
+import Daemon from './Daemon';
+import Magician from './Magician';
+import Swordsman from './Swordsman';
+import Undead from './Undead';
+import Zombie from './Zombie';
+
+console.log('worked');
+
+const player1 = new Bowerman('Legolas', 'Bowerman');
+const player2 = new Daemon('Lucifer', 'Daemon');
+const player3 = new Magician('Merlin', 'Magician');
+const player4 = new Swordsman('Arthur', 'Swordsman');
+const player5 = new Undead('Crispy', 'Undead');
+const player6 = new Zombie('Shawn', 'Zombie');
+console.log(player1,player2,player3,player4,player5,player6);
